7e52ac4f980281d702e81c4bf7c365a2a07775f0
CodeBurner <106039683+codeBurner0@users.noreply.github.com>
2023-05-18 15:50:33 +0530

    Time: 9 ms (99.36%), Space: 41.7 MB (93.04%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,27 @@
 class Solution {
+    // public boolean isPalindrome(int x) {
+    //     if(x<0){
+    //         return false;
+    //     }
+    //     int n=x;
+    //     int rev=0;
+    //     while(n>0){
+    //         int ld=n%10;
+    //         rev=rev*10+ld;
+    //         n=n/10;
+    //     }
+    //     return rev==x;
+    // }
     public boolean isPalindrome(int x) {
-        if(x<0){
+        if(x<0 || x!=0 && x%10==0){
             return false;
         }
-        int n=x;
         int rev=0;
-        while(n>0){
-            int ld=n%10;
+        while(x>rev){
+            int ld=x%10;
             rev=rev*10+ld;
-            n=n/10;
+            x=x/10;
         }
-        return rev==x;
+        return (rev==x || x==rev/10);
     }
 }
